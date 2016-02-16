d4cc4615e5320d027184f60b8ad883e8deebe5fc
rodrigobezerra <rodrigo.pinheirob@gmail.com>
2016-02-16 09:13:22 -0400

    testes com alert, snackbar e toast

--- MeusAlertas/app/src/main/java/com/rodrigobezerra/meusalertas/MainActivity.java
+++ MeusAlertas/app/src/main/java/com/rodrigobezerra/meusalertas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.rodrigobezerra.meusalertas;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -26,8 +28,38 @@ public class MainActivity extends AppCompatActivity {
         // que por exemplo para este caso seria a propria 'MainActivity.this';
         Toast.makeText(getApplicationContext(), "Eu sou o toast também", Toast.LENGTH_SHORT).show();
 
-        Snackbar snackbar = Snackbar.make(v, "Eu sou o snackbar", Snackbar.LENGTH_SHORT);
+        Snackbar snackbar;
+        snackbar =  Snackbar.make(v, "Eu sou o snackbar", Snackbar.LENGTH_SHORT);
         snackbar.show();
-        //snackbar.setAction();
+
+        snackbar.setAction("Snack Action", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("LogX", "Clicou no snack!!!");
+            }
+        });
+
+        AlertDialog.Builder construtorAlerta;
+        construtorAlerta = new AlertDialog.Builder(this);
+        construtorAlerta.setTitle("Titulo de alerta");
+        construtorAlerta.setMessage("Qual é a sua opção?");
+
+        construtorAlerta.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("LogX", "Clicou no sim!!!");
+            }
+        });
+
+        construtorAlerta.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("LogX", "Clicou no não!!!");
+            }
+        });
+
+        AlertDialog meuAlerta = construtorAlerta.create();
+        meuAlerta.show();
     }
+
 }
