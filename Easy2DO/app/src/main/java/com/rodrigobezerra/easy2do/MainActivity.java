4f6711df4c7c6da9b5b77753b68aef56e8f74dcc
rodrigobezerra <rodrigo.pinheirob@gmail.com>
2016-06-29 17:57:53 -0400

    Ajustes e config do banco

--- Easy2DO/app/src/main/java/com/rodrigobezerra/easy2do/MainActivity.java
+++ Easy2DO/app/src/main/java/com/rodrigobezerra/easy2do/MainActivity.java
@@ -1,52 +1,86 @@
 package com.rodrigobezerra.easy2do;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText meuTexto;
+    private ListView minhaLista;
+    private Button meuBotao;
+
+    private SQLiteDatabase bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        meuTexto = (EditText) findViewById(R.id.editText);
+        minhaLista = (ListView) findViewById(R.id.listView);
+        meuBotao = (Button) findViewById(R.id.button);
+
+        meuBotao.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void carregaTarefas() {
+        try {
+            // criacao da tabela 'tarefas'
+            bd = openOrCreateDatabase("TODOList", MODE_PRIVATE, null);
+            bd.execSQL("CREATE TABLE IF NOT EXISTS tarefas" +
+                    "(id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // criando um novo registro de tarefa
+            String novaTarefa = meuTexto.getText().toString();
+            if (!novaTarefa.trim().equals(""))
+                bd.execSQL("INSERT INTO tarefas(tarefa) VALUES ('"+ novaTarefa +"')");
+            else
+                Log.d("logX","A nova tarefa está vazia!");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Listando os dados
+            Cursor cursor = bd.rawQuery("SELECT * FROM tarefas", null);
+
+            int indiceColunaID = cursor.getColumnIndex("id");
+            int indiceColunaTarefa = cursor.getColumnIndex("tarefa");
+
+            cursor.moveToFirst();
+            while (cursor != null) {
+                Log.i("logX", "ID: "+ cursor.getString(indiceColunaID) +
+                        "| Tarefa: " + cursor.getString(indiceColunaTarefa));
+                cursor.moveToNext();
+            }
+
+        } catch (Exception e) {
+            Log.e("logX",e.getMessage().toString());
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void adcTarefa() {
+
+
+    }
+
+    private void apagarTarefa() {
+
     }
 }
